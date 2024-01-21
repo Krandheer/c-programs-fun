b14ac8e0f33b06f34fe7d7125ecdb68ba8fdf10a
randheer <randheer.dakshana15@gmail.com>
2024-01-21 18:18:40 +0530

    input in c

--- example2.c
+++ example2.c
@@ -4,6 +4,6 @@ int main() {
   int usf, euf;
   printf("Enter floor number\n");
   scanf("%d", &usf);
-    euf = usf-1;
-    printf("Floor is %d\n", euf);
+  euf = usf - 1;
+  printf("Floor is %d\n", euf);
 }
