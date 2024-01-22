425e7ec9e050cb4d3b679203e1f1251285360014
randheer <randheer.dakshana15@gmail.com>
2024-01-22 12:56:03 +0530

    another code in c

--- temp.c
+++ temp.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 int main() {
-  int lower = 0;
-  int upper = 300;
-  float celsius;
-  float farh;
-  while (lower <= upper) {
 
-    celsius = (5.0 / 9.0) * (lower - 32.0);
-    farh = lower;
-    printf("%4.0f, %6.1f\n", farh, celsius);
-    lower += 20;
+  for (int i = 0; i <= 300; i = i + 20) {
+    printf("%4d, %6.1f \n", i, (5.0 / 9.0) * (i - 32));
   }
+  // int lower = 0;
+  // int upper = 300;
+  // float celsius;
+  // float farh;
+  // while (lower <= upper) {
+  //
+  //   celsius = (5.0 / 9.0) * (lower - 32.0);
+  //   farh = lower;
+  //   printf("%4.0f, %6.1f\n", farh, celsius);
+  //   lower += 20;
+  // }
 }
