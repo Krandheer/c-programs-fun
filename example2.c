1c5b517a7f7fdb703d1cf78d359f43e3b54e0188
randheer <randheer.dakshana15@gmail.com>
2024-01-22 21:28:28 +0530

    call of function inside main function

--- example2.c
+++ example2.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
+int power(int x, int n) {
+  int p = 1;
+  for (int i = 0; i < n; i++) {
+    p = x * p;
+  }
+  return p;
+}
+
 int main() {
   int usf, euf;
   printf("Enter floor number\n");
   scanf("%d", &usf);
-  euf = usf - 1;
   printf("Floor is %d\n", euf);
+
+  int a = 2;  int b = 3;
+  printf("%d", power(a, b));
 }
