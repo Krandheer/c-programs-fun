5c32b623874aca3185f426e61383a74c11f563eb
randheer <randheer.dakshana15@gmail.com>
2024-02-07 14:02:32 +0530

    pointers understood perfectly

--- pointers.c
+++ pointers.c
@@ -2,15 +2,16 @@
 
 void func(int a, int *b) {
   a = 1;
+  printf("address of y that is passed to reference b is: %p\n", b);
   *b = 2; /* reference to a memory address where w is being put */
   printf("called from func\n");
-  printf("%d %p\n", a, &b);
+  printf("%d adress of reference b is: %p\n", a, &b);
 }
 int main() {
   int x = 42;
   int y = 43;
   printf("x: %d, y: %d\n", x, y);
-  printf("%p\n", &y);
+  printf("address of y is: %p\n", &y);
   // passing the address of y to func where b get the reference of address of y
   // and then modifies it to 2
   func(x, &y);
