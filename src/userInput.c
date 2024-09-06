4d4a0f51612d718d4df7c2c45534a637b50e0665
randheer <randheer.dakshana15@gmail.com>
2024-09-07 01:15:53 +0530

    take input from user

--- src/userInput.c
+++ src/userInput.c
@@ -2,8 +2,14 @@
 
 int main() {
   int age;
+  char name[25];
+
+  printf("What is your name: ");
+  scanf("%s", name);
+
   printf("Enter you age: ");
   scanf("%d", &age);
-  printf("%d\n", age);
+
+  printf("you name is %s and you age is %d\n", name, age);
   return 0;
 }
