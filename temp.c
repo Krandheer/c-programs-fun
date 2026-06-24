480a41698908fe5d48c202b8a44594860b0a1de3
randheer <randheer.dakshana15@gmail.com>
2026-06-24 20:50:21 +0530

    c

--- temp.c
+++ temp.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
 
 int main() {
-
-  for (int i = 0; i <= 300; i = i + 20) {
-    printf("%4d, %6.1f \n", i, (5.0 / 9.0) * (i - 32));
-  }
-  // int lower = 0;
-  // int upper = 300;
-  // float celsius;
-  // float farh;
-  // while (lower <= upper) {
-  //
-  //   celsius = (5.0 / 9.0) * (lower - 32.0);
-  //   farh = lower;
-  //   printf("%4.0f, %6.1f\n", farh, celsius);
-  //   lower += 20;
-  // }
+  char name[16];
+  printf("what is your name: ");
+  scanf("%s", name);
+  printf("hello %s\n", name);
+  return 0;
 }
