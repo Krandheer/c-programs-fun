16d5315d06148f4b7069ccf63ec17a5aacc765e7
randheer <randheer.dakshana15@gmail.com>
2026-07-02 23:37:04 +0530

    reading file in c

---
+++ read_io.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void read_char() {
+  FILE *fp;
+  fp = fopen("hello.txt", "r");
+
+  int c;
+  while ((c = fgetc(fp)) != EOF) {
+    printf("%c", c);
+  }
+}
+
+void read_by_line() {
+  FILE *fp;
+  fp = fopen("para.txt", "r");
+  char s[1024];
+  while ((fgets(s, sizeof(s), fp)) != NULL) {
+    printf("%s", s);
+  }
+}
+
+int main() {
+  // read_char();
+  read_by_line();
+  return 0;
+}
